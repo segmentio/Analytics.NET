7abdd9c788291a2d3aebae0e411c818e54094907
ivolo <ivolo@mit.edu>
2014-05-09 16:22:04 -0400

    updating batch

--- Analytics/Model/Batch.cs
+++ Analytics/Model/Batch.cs
@@ -8,24 +8,19 @@ namespace Segmentio.Model
 {
     internal class Batch
     {
-
-        [JsonProperty(PropertyName = "writeKey")]
-        internal string WriteKey { get; set; }
-
         [JsonProperty(PropertyName = "batch")]
         internal List<BaseAction> batch { get; set; }
-	
-	[JsonProperty(PropertyName = "context")]
-	internal Context context { get; set; }
+		
+		[JsonProperty(PropertyName = "context")]
+		internal Context context { get; set; }
 
-        internal Batch() { 
-		this.context = new Context ();
-		this.context.Add ("library", "analytics-.NET");
-	}
+      	internal Batch() { 
+			this.context = new Context ();
+			this.context.Add ("library", "analytics-.NET");
+		}
 
-        internal Batch(string writeKey, List<BaseAction> batch) : this()
+        internal Batch(List<BaseAction> batch) : this()
         {
-            this.WriteKey = writeKey;
             this.batch = batch;
         }
     }

48cdc6b338a8857d86130ebd840eea4b34b9bf06
Kevin <kevin.gilliard8415@gmail.com>
2017-09-20 11:12:29 -0700

    Change nuget package info

---
+++ AnalyticsTest_NetStandard/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnalyticsTest_NetStandard
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}

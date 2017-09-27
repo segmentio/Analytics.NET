91098e30c4ca961e7ed2d8fde0c4a76925a39558
Kevin <kevin.gilliard8415@gmail.com>
2017-09-27 15:44:23 -0700

    Update package version to 0.1.5 and add API testing code.

--- Test.Net35/Program.cs
+++ Test.Net35/Program.cs
@@ -11,10 +11,20 @@ namespace Test.Net35
 	{
 		static void Main(string[] args)
 		{
-			Analytics.Initialize(Segment.Test.Constants.WRITE_KEY);
+			Logger.Handlers += Logger_Handlers;
 
-			FlushTests tests = new FlushTests();
-			tests.PerformanceTestNet35();
+			//Analytics.Initialize(Segment.Test.Constants.WRITE_KEY);
+
+			//FlushTests tests = new FlushTests();
+			//tests.PerformanceTestNet35();
+			Analytics.Initialize("nAZ4rSBdQMoMJ6bswze53Jorbpjtne78");
+			Analytics.Client.Track("prateek", "Item Purchased (with .Net 3.5)");
+			Analytics.Client.Flush();
+		}
+
+		private static void Logger_Handlers(Logger.Level level, string message, IDictionary<string, object> args)
+		{
+			Console.WriteLine(message);
 		}
 	}
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -11,10 +11,20 @@ namespace Segment.Test
 	{
 		static void Main(string[] args)
 		{
-			Analytics.Initialize(Segment.Test.Constants.WRITE_KEY);
+			Logger.Handlers += Logger_Handlers;
 
-			FlushTests tests = new FlushTests();
-			tests.PerformanceTest().Wait();
+			//Analytics.Initialize(Segment.Test.Constants.WRITE_KEY);
+
+			//FlushTests tests = new FlushTests();
+			//tests.PerformanceTest().Wait();
+			Analytics.Initialize("nAZ4rSBdQMoMJ6bswze53Jorbpjtne78");
+			Analytics.Client.Track("prateek", "Item Purchased");
+			Analytics.Client.Flush();
+		}
+
+		private static void Logger_Handlers(Logger.Level level, string message, IDictionary<string, object> args)
+		{
+			Console.WriteLine(message);
 		}
 	}
 }

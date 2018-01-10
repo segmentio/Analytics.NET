249b7b1f53bbfd1aa374d1df7d1594f7054da3ca
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-01-10 11:55:51 -0800

    Add test project for e2e test

---
+++ Test.E2E/AxiosClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segment.E2ETest
+{
+    class AxiosClient
+    {
+        HttpClient client = null;
+        int RetryCount = 1;
+
+        public AxiosClient(string BaseAddress, int timeout, string authorization = "")
+        {
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://api.runscope.com"),
+                Timeout = new TimeSpan(0, 0, 0, 0, timeout),
+            };
+
+            // Set authorization header
+            if (!string.IsNullOrEmpty(authorization))
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + authorization);
+
+            // Set retry count
+            this.RetryCount = 1;
+        }
+
+        public void SetRetryCount(int retries)
+        {
+            this.RetryCount = retries < 1 ? 1 : retries;
+        }
+
+        public async Task<HttpResponseMessage> Get(string url)
+        {
+            for (int i = 0; i < this.RetryCount; i++)
+            {
+                try
+                {
+                    return await this.client.GetAsync(url);
+                }
+                catch (System.Exception /*ex*/)
+                {
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Test.E2E/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Segment.E2ETest
+{
+	public class Constants
+	{
+		// project segmentio/dotnet-test
+		public static string WRITE_KEY = "Cum1GHo14CegfLd83gVxsbc5Sb6jK8SN";
+
+		public static string RUNSCOPE_BUCKET = "pwb8mcmfks0f";
+	}
+}
+

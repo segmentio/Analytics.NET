85516dc6ee6a3b4b935aabfe8280952183f7275d
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-09-18 21:37:14 -0400

    Run analytics with system environment variables

--- Test.E2E/e2e.sh
+++ Test.E2E/e2e.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
- set -ex
- if [ "$RUN_E2E_TESTS" != "true" ]; then
+set -ex
+if [ "$RUN_E2E_TESTS" != "true" ]; then
   echo "Skipping end to end tests."
 else
   echo "Running end to end tests..."
   wget https://github.com/segmentio/library-e2e-tester/releases/download/0.2.1/tester_linux_amd64 -O tester
   chmod +x tester
-  ./tester -path='dotnet run -- '
+  chmod +x analytics.sh
+
+  # Hide system environment variable values
+  echo "./tester -path='./analytics.sh'"
+  { ./tester -path='./analytics.sh' -segment-write-key="$SEGMENT_WRITE_KEY" -webhook-auth-username="$WEBHOOK_AUTH_USERNAME" -webhook-bucket="$WEBHOOK_BUCKET"; } 2> /dev/null
 
   # Run code coverage
-  curl https://codecov.io/bash --output codecov
-  chmod 777 codecov
+  wget https://codecov.io/bash -O codecov
+  chmod +x codecov
   ./codecov
 
   echo "End to end tests completed!"

11162ac89697ca00fb73c20c7e184817c9ea634b
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-09-27 08:29:33 -0400

    Set execute permission with git

--- Test.E2E/e2e.sh
+++ Test.E2E/e2e.sh
@@ -6,7 +6,6 @@ else
   echo "Running end to end tests..."
   wget https://github.com/segmentio/library-e2e-tester/releases/download/0.2.1/tester_linux_amd64 -O tester
   chmod +x tester
-  chmod +x analytics.sh
 
   # Hide system environment variable values
   echo "./tester -path='./analytics.sh'"

84cc28ba93b5510b1cf857d6ab937054d06278fa
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-10-11 17:43:58 -0400

    Add new test method for retry network errors

--- Test.UniversalApp/ConnectionTests.cs
+++ Test.UniversalApp/ConnectionTests.cs
@@ -1,57 +1,83 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Segment.Test
 {
-	[TestClass]
-	public class ConnectionTests
-	{
-		[TestInitialize]
-		public void Init()
-		{
-			Analytics.Dispose();
-			Logger.Handlers += LoggingHandler;
-		}
+    [TestClass]
+    public class ConnectionTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Analytics.Dispose();
+            Logger.Handlers += LoggingHandler;
+        }
 
-		[TestMethod]
-		public void ProxyTestNetPortable()
-		{
-			// Set proxy address, like as "http://localhost:8888"
-			Analytics.Initialize(Constants.WRITE_KEY, new Config().SetAsync(false).SetProxy(""));
+        [TestMethod]
+        public void RetryErrorTestPortable()
+        {
+            Stopwatch watch = new Stopwatch();
 
-			Actions.Identify(Analytics.Client);
+            // Set invalid host address and make timeout to 1s
+            var config = new Config().SetAsync(false);
+            config.SetHost("https://api.segment.com");
+            config.SetTimeout(new TimeSpan(0, 0, 1));
+            Analytics.Initialize(Constants.WRITE_KEY, config);
 
-			Assert.AreEqual(1, Analytics.Client.Statistics.Submitted);
-			Assert.AreEqual(1, Analytics.Client.Statistics.Succeeded);
-			Assert.AreEqual(0, Analytics.Client.Statistics.Failed);
-		}
+            // Calculate working time for Identiy message with invalid host address
+            watch.Start();
+            Actions.Identify(Analytics.Client);
+            watch.Stop();
 
-		[TestMethod]
-		public void GZipTestNetPortable()
-		{
-			// Set proxy address, like as "http://localhost:8888"
-			Analytics.Initialize(Constants.WRITE_KEY, new Config().SetAsync(false).SetRequestCompression(true));
+            Assert.AreEqual(1, Analytics.Client.Statistics.Submitted);
+            Assert.AreEqual(0, Analytics.Client.Statistics.Succeeded);
+            Assert.AreEqual(1, Analytics.Client.Statistics.Failed);
 
-			Actions.Identify(Analytics.Client);
+            // Handling Identify message will take more than 10s even though the timeout is 1s.
+            // That's because it retries submit when it's failed.
+            Assert.AreEqual(true, watch.ElapsedMilliseconds > 10000);
+        }
 
-			Assert.AreEqual(1, Analytics.Client.Statistics.Submitted);
-			Assert.AreEqual(1, Analytics.Client.Statistics.Succeeded);
-			Assert.AreEqual(0, Analytics.Client.Statistics.Failed);
-		}
+        [TestMethod]
+        public void ProxyTestNetPortable()
+        {
+            // Set proxy address, like as "http://localhost:8888"
+            Analytics.Initialize(Constants.WRITE_KEY, new Config().SetAsync(false).SetProxy(""));
 
-		static void LoggingHandler(Logger.Level level, string message, IDictionary<string, object> args)
-		{
-			if (args != null)
-			{
-				foreach (string key in args.Keys)
-				{
-					message += String.Format(" {0}: {1},", "" + key, "" + args[key]);
-				}
-			}
-			Debug.WriteLine(String.Format("[ConnectionTests] [{0}] {1}", level, message));
-		}
-	}
+            Actions.Identify(Analytics.Client);
+
+            Assert.AreEqual(1, Analytics.Client.Statistics.Submitted);
+            Assert.AreEqual(1, Analytics.Client.Statistics.Succeeded);
+            Assert.AreEqual(0, Analytics.Client.Statistics.Failed);
+        }
+
+        [TestMethod]
+        public void GZipTestNetPortable()
+        {
+            // Set proxy address, like as "http://localhost:8888"
+            Analytics.Initialize(Constants.WRITE_KEY, new Config().SetAsync(false).SetRequestCompression(true));
+
+            Actions.Identify(Analytics.Client);
+
+            Assert.AreEqual(1, Analytics.Client.Statistics.Submitted);
+            Assert.AreEqual(1, Analytics.Client.Statistics.Succeeded);
+            Assert.AreEqual(0, Analytics.Client.Statistics.Failed);
+        }
+
+        static void LoggingHandler(Logger.Level level, string message, IDictionary<string, object> args)
+        {
+            if (args != null)
+            {
+                foreach (string key in args.Keys)
+                {
+                    message += String.Format(" {0}: {1},", "" + key, "" + args[key]);
+                }
+            }
+            Debug.WriteLine(String.Format("[ConnectionTests] [{0}] {1}", level, message));
+        }
+    }
 }

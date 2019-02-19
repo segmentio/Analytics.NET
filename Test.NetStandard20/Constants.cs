feeefdddc444c19e6222e1a1b12ed2deefb92cfb
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2019-02-20 05:56:51 +0800

    Update write key

--- Test.NetStandard20/Constants.cs
+++ Test.NetStandard20/Constants.cs
@@ -5,7 +5,7 @@ namespace Segment.Test
     public class Constants
     {
         // project segmentio/dotnet-test
-        public static string WRITE_KEY = "r7bxis28wy";
+        public static string WRITE_KEY = "Cum1GHo14CegfLd83gVxsbc5Sb6jK8SN";
     }
 }
 
--- Test/Constants.cs
+++ Test/Constants.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace Segment.Test
 {
     public class Constants
     {
         // project segmentio/dotnet-test
-        public static string WRITE_KEY = "r7bxis28wy";
+        public static string WRITE_KEY = "Cum1GHo14CegfLd83gVxsbc5Sb6jK8SN";
     }
 }
 

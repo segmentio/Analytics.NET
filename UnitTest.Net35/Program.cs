a4b39bbedb678dcfe3497429f1b7265b6f882d7e
Rodrigo Miranda <rmiranda@applaudostudios.com>
2019-09-16 15:34:01 -0600

    LIB-833 Add Console C# test project to the solution

---
+++ UnitTest.Net35/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTest.Net35
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}

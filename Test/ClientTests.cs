edea9a485b97b3a51718130b953b2ae3c7436bfe
Carlos Galan <cgalan@applaudostudios.com>
2020-01-21 17:20:38 -0600

    Modifying .editorconfig to use space instead of tab

--- Test/ClientTests.cs
+++ Test/ClientTests.cs
@@ -8,36 +8,35 @@ using Segment.Model;
 
 namespace Segment.Test
 {
-	[TestFixture()]
-	public class ClientTests
-	{
-		Client client;
+    [TestFixture()]
+    public class ClientTests
+    {
+        Client client;
 
-		[SetUp]
-		public void Init()
-		{
-			client = new Client("foo");
-		}
+        [SetUp]
+        public void Init()
+        {
+            client = new Client("foo");
+        }
 
-		[Test]
-		public void InitilizationThrowsInvalidOperationExceptionWhenWriteKeyIsEmpty()
-		{
-			var ex = Assert.Throws<InvalidOperationException>(() => new Client(""));
+        [Test]
+        public void InitilizationThrowsInvalidOperationExceptionWhenWriteKeyIsEmpty()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => new Client(""));
 
-			Assert.AreEqual("Please supply a valid writeKey to initialize.", ex.Message);
+            Assert.AreEqual("Please supply a valid writeKey to initialize.", ex.Message);
+        }
 
-		}
+        [Test]
+        public void PageThrowsInvalidOperationExceptionWhenPageNameIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Page("userId", ""));
 
-		[Test]
-		public void PageThrowsInvalidOperationExceptionWhenPageNameIsEmtpy()
-		{
-			var ex = Assert.Throws<InvalidOperationException>(() => client.Page("userId", ""));
+            Assert.AreEqual("Please supply a valid name to call #Page.", ex.Message);
+        }
 
-			Assert.AreEqual("Please supply a valid name to call #Page.", ex.Message);
-		}
-
-		[Test ()]
-        public void TrackTest ()
+        [Test()]
+        public void TrackTest()
         {
             // verify it doesn't fail for a null options
             client.Screen("bar", "qaz", null, null);

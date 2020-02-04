25bf9b469447aacbb80e284e9ee8444ce753ffc2
Carlos Galan <cgalan@applaudostudios.com>
2020-02-04 16:48:33 -0600

    LAPP-13 Adding test for statisctics

---
+++ Test.NetStandard20/Stats/StatisticsTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using Segment.Stats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Segment.NetStandard20.Stats
+{
+    [TestFixture]
+    public class StatisticsTests
+    {
+        Statistics _statistics;
+        [SetUp]
+        public void Init()
+        {
+            _statistics = new Statistics();
+        }
+
+        [Test]
+        public void IncrementStatisticWorks()
+        {
+            _statistics.IncrementSubmitted();
+            _statistics.IncrementSubmitted();
+            _statistics.IncrementSubmitted();
+            _statistics.IncrementSucceeded();
+            _statistics.IncrementSucceeded();
+            _statistics.IncrementFailed();
+
+            Assert.AreEqual(1, _statistics.Failed);
+            Assert.AreEqual(2, _statistics.Succeeded);
+            Assert.AreEqual(3, _statistics.Submitted);
+        }
+
+        [Test]
+        public void IncrementSetToZeroWhenStatisticHasReachedMaxValue()
+        {
+            SetIntMaxValueToStatistic("_submitted");
+            SetIntMaxValueToStatistic("_succeeded");
+            SetIntMaxValueToStatistic("_failed");
+
+            _statistics.IncrementSubmitted();
+            _statistics.IncrementSucceeded();
+            _statistics.IncrementFailed();
+
+            Assert.AreEqual(0, _statistics.Failed);
+            Assert.AreEqual(0, _statistics.Succeeded);
+            Assert.AreEqual(0, _statistics.Submitted);
+        }
+
+        private void SetIntMaxValueToStatistic(string field)
+        {
+            var type = _statistics.GetType();
+            var prop = type.GetField(field,
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            prop.SetValue(_statistics, int.MaxValue);
+
+        }
+    }
+}

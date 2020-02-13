5ab0a31d581c295a713869336f5efd449af631ed
Carlos Galan <cgalan@applaudostudios.com>
2020-02-13 11:15:47 -0600

    LAPP-19 updating minicover tool and excluding Test.NetStandard20 from the report result

--- Test.NetStandard20/coverage_report.sh
+++ Test.NetStandard20/coverage_report.sh
@@ -3,7 +3,7 @@ set -ex
 
 dotnet restore
 dotnet build
-dotnet minicover instrument --workdir ../ --assemblies Test.NetStandard20/**/bin/**/*.dll --sources /**/*.cs
+dotnet minicover instrument --workdir ../ --assemblies Analytics.NetStandard20/**/bin/**/*.dll --sources /**/*.cs
 dotnet minicover reset
 dotnet test --no-build
 dotnet minicover opencoverreport --workdir ../ --threshold 80

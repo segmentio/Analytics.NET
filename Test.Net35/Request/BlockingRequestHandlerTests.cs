4553edd2b8184df26a7bd57135a9621039ec4e2c
Carlos Galan <cgalan@applaudostudios.com>
2020-02-25 16:16:10 -0600

    LAPP-20 Adding tests for retry strategy on Net35

--- Test.Net35/Request/BlockingRequestHandlerTests.cs
+++ Test.Net35/Request/BlockingRequestHandlerTests.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 
-namespace Test.Net35.Request
+namespace Segment.Test.Request
 {
     [TestFixture]
     public class BlockingRequestHandlerTests
@@ -34,14 +34,15 @@ namespace Test.Net35.Request
 
 
         [Test]
-        public void RequestIncludesRequiredHeaders() {
+        public void RequestIncludesRequiredHeaders()
+        {
             var batch = GetBatch();
 
             _handler.MakeRequest(batch).GetAwaiter().GetResult();
 
             _mockHeaders.Verify(x => x.Set("Authorization", It.IsAny<string>()), Times.Once);
             _mockHeaders.Verify(x => x.Set("Content-Type", "application/json; charset=utf-8"), Times.Once);
-        } 
+        }
 
         [Test]
         public void MakeRequestWith200StatusCode()
@@ -62,10 +63,7 @@ namespace Test.Net35.Request
         public void MakeRequestWith5xxStatusCode()
         {
             var batch = GetBatch();
-            HttpWebResponse response = Mock.Of<HttpWebResponse>(x => x.StatusCode == HttpStatusCode.InternalServerError);
-            _mockHttpClient
-                .Setup(x => x.UploadData(It.IsAny<Uri>(), "POST", It.IsAny<byte[]>()))
-                .Throws(new WebException("", null, WebExceptionStatus.UnknownError, response));
+            ClientThrowWebException(HttpStatusCode.InternalServerError);
 
             _handler.MakeRequest(batch).GetAwaiter().GetResult();
 
@@ -73,15 +71,30 @@ namespace Test.Net35.Request
             Assert.AreEqual(1, _client.Statistics.Failed);
             AssertSendAsyncWasCalled(7);
         }
+
+        [Test]
+        public void MakeRequestWith429StatusCode()
+        {
+            //Arrange
+            var batch = GetBatch();
+            ClientThrowWebException((HttpStatusCode)429);
+
+            //Act            
+            _handler.MakeRequest(batch).GetAwaiter().GetResult();
+
+            //Assert
+            Assert.AreEqual(0, _client.Statistics.Succeeded);
+            Assert.AreEqual(1, _client.Statistics.Failed);
+            AssertSendAsyncWasCalled(7);
+        }
+
+
         [Test]
         public void MakeRequestWith4xxStatusCode()
         {
             //Arrange
-            HttpWebResponse response = Mock.Of<HttpWebResponse>(x => x.StatusCode == HttpStatusCode.NotFound);
-            _mockHttpClient
-                .Setup(x => x.UploadData(It.IsAny<Uri>(), "POST", It.IsAny<byte[]>()))
-                .Throws(new WebException("", null, WebExceptionStatus.UnknownError, response));
             var batch = GetBatch();
+            ClientThrowWebException(HttpStatusCode.NotFound);
 
             //Act
             _handler.MakeRequest(batch).GetAwaiter().GetResult();
@@ -91,7 +104,70 @@ namespace Test.Net35.Request
             Assert.AreEqual(1, _client.Statistics.Failed);
             AssertSendAsyncWasCalled(1);
         }
+        [Test]
+        public void MakeRequestWithErrorStatusCodeRetryUntilSuccess()
+        {
+            //Arrange
+            MultipleHttpResponseBehavior(
+                HttpStatusCode.InternalServerError,
+                HttpStatusCode.NotImplemented,
+                (HttpStatusCode)429,
+                new byte[] { });
 
+            var batch = GetBatch();
+
+            //Act
+            _handler.MakeRequest(batch).GetAwaiter().GetResult();
+
+            //Assert
+            Assert.AreEqual(1, _client.Statistics.Succeeded);
+            Assert.AreEqual(0, _client.Statistics.Failed);
+            AssertSendAsyncWasCalled(4);
+        }
+        [Test]
+        public void MakeRequestWithErrorStatusCodeRetryUntil4xxErrorExcept429()
+        {
+            //Arrange
+            MultipleHttpResponseBehavior(
+                HttpStatusCode.InternalServerError,
+                HttpStatusCode.NotImplemented,
+                (HttpStatusCode)429,
+                HttpStatusCode.NotFound);
+
+            var batch = GetBatch();
+
+            //Act
+            _handler.MakeRequest(batch).GetAwaiter().GetResult();
+
+            //Assert
+            Assert.AreEqual(0, _client.Statistics.Succeeded);
+            Assert.AreEqual(1, _client.Statistics.Failed);
+            AssertSendAsyncWasCalled(4);
+        }
+
+
+        private void ClientThrowWebException(HttpStatusCode httpStatusCode)
+        {
+            HttpWebResponse response = Mock.Of<HttpWebResponse>(x => x.StatusCode == httpStatusCode);
+            _mockHttpClient
+                .Setup(x => x.UploadData(It.IsAny<Uri>(), "POST", It.IsAny<byte[]>()))
+                .Throws(new WebException("", null, WebExceptionStatus.UnknownError, response));
+        }
+
+        private void MultipleHttpResponseBehavior(params object[] results)
+        {
+            var seq = _mockHttpClient.SetupSequence(x => x.UploadData(It.IsAny<Uri>(), "POST", It.IsAny<byte[]>()));
+            foreach (var r in results)
+            {
+                if (r is HttpStatusCode httpStatusCode)
+                {
+                    HttpWebResponse response = Mock.Of<HttpWebResponse>(x => x.StatusCode == httpStatusCode);
+                    seq.Throws(new WebException("", null, WebExceptionStatus.UnknownError, response));
+                }
+                else if (r is byte[] wr)
+                    seq.Returns(wr);
+            }
+        }
 
         private void AssertSendAsyncWasCalled(int times = 1)
         {

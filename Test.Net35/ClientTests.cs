3b7981007c601bf49517cd37288db2062393ad22
Carlos Galan <cgalan@applaudostudios.com>
2020-02-25 16:14:39 -0600

    LAPP-20 improving code coverage on Client

--- Test.Net35/ClientTests.cs
+++ Test.Net35/ClientTests.cs
@@ -19,6 +19,63 @@ namespace Segment.Test
             client = new Client("foo");
         }
 
+
+        [Test]
+        public void InitilizationThrowsInvalidOperationExceptionWhenWriteKeyIsEmpty()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => new Client(""));
+            Assert.AreEqual("Please supply a valid writeKey to initialize.", ex.Message);
+        }
+
+        [Test]
+        public void PageThrowsInvalidOperationExceptionWhenPageNameIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Page("userId", ""));
+
+            Assert.AreEqual("Please supply a valid name to call #Page.", ex.Message);
+        }
+
+        [Test]
+        public void PageThrowsInvalidOperationExceptionWhenUserIdIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Page("", ""));
+
+            Assert.AreEqual("Please supply a valid userId or anonymousId to call #Page.", ex.Message);
+        }
+
+        [Test]
+        public void AliasThrowsInvalidOperationExceptionWhenUserIdIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Alias("previousId", ""));
+
+            Assert.AreEqual("Please supply a valid 'userId' to Alias.", ex.Message);
+        }
+
+        [Test]
+        public void AliasThrowsInvalidOperationExceptionWhenPreviousIdIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Alias("", ""));
+
+            Assert.AreEqual("Please supply a valid 'previousId' to Alias.", ex.Message);
+        }
+
+        [Test]
+        public void GroupThrowsInvalidOperationExceptionWhenGroupIdIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Group("userId", "", new Options()));
+
+            Assert.AreEqual("Please supply a valid groupId to call #Group.", ex.Message);
+        }
+
+        [Test]
+        public void GroupThrowsInvalidOperationExceptionWhenUserIdIsEmtpy()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => client.Group("", "", (Options)null));
+
+            Assert.AreEqual("Please supply a valid userId or anonymousId to call #Group.", ex.Message);
+        }
+
+
         [Test ()]
         public void TrackTestNet35 ()
         {

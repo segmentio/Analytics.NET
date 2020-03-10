0d7054655e7848c03c87e38ab8f674829a02867a
Carlos Galan <cgalan@applaudostudios.com>
2020-03-10 15:15:09 -0600

    LAPP-22 Refactoring backoff strategy and creating a separated class

---
+++ Test.NetStandard20/Request/BackoTests.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Segment.Request;
+
+namespace Test.NetStandard20.Request
+{
+    [TestFixture]
+    class BackoTests
+    {
+        [TestCase(1, ExpectedResult = 100)]
+        [TestCase(2, ExpectedResult = 200)]
+        [TestCase(3, ExpectedResult = 400)]
+        public int TimeShouldIncreaseExponentially(int calls)
+        {
+            //Arrange
+            var backo = new Backo(jitter: 0);
+
+            //Act
+            for (var i = 0; i < calls - 1; i++)
+                backo.AttemptTime();
+            var time = backo.AttemptTime();
+
+            //Assert
+            return time;
+        }
+
+        [TestCase(1, 500, ExpectedResult = 100)]
+        [TestCase(3, 500, ExpectedResult = 400)]
+        [TestCase(4, 500, ExpectedResult = 500)]
+        public int TimeShouldBeLessOrEqualToMax(int calls, int max)
+        {
+            //Arrange
+            var backo = new Backo(jitter: 0, max: max);
+
+            //Act
+            for (var i = 0; i < calls - 1; i++)
+                backo.AttemptTime();
+
+            var time = backo.AttemptTime();
+
+            //Assert
+            return time;
+        }
+
+        [TestCase(3, 500,ExpectedResult = 2000)]
+        [TestCase(3, 250, ExpectedResult = 1000)]
+        [TestCase(3, 75, ExpectedResult = 300)]
+        public int TimeShouldDependOnMin(int calls, int min)
+        {
+            //Arrange
+            var backo = new Backo(jitter: 0, min: min);
+
+            //Act
+            for (var i = 0; i < calls - 1; i++)
+                backo.AttemptTime();
+
+            var time = backo.AttemptTime();
+
+            //Assert
+            return time;
+        }
+    }
+}

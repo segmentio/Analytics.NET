dc064b05882a65a88b80657bcd7691479e951acf
Carlos Galan <cgalan@applaudostudios.com>
2020-05-11 16:05:09 -0600

    Move default values to optional parameters in the config class

--- Analytics/Config.cs
+++ Analytics/Config.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Segment.Model;
 
 namespace Segment
 {
@@ -47,28 +48,34 @@ namespace Segment
         /// <param name="gzip">Compress data w/ gzip before dispatch</param>
         /// <param name="userAgent">Sets User Agent Header</param>
         public Config(
-            string host = null,
+            string host = "https://api.segment.io",
             string proxy = null,
             TimeSpan? timeout = null,
-            int? maxQueueSize = null,
-            int? flushAt = null,
-            bool? async = null,
-            int? threads = null,
-            double? flushInterval = null,
-            bool? gzip = null,
+            int maxQueueSize = 10000,
+            int flushAt = 20,
+            bool async = true,
+            int threads = 1,
+            double flushInterval = 10,
+            bool gzip = false,
             string userAgent = null
             )
         {
-            this.Host = host ?? Defaults.Host;
+            this.Host = host;
             this.Proxy = proxy ?? "";
-            this.Timeout = timeout ?? Defaults.Timeout;
-            this.MaxQueueSize = maxQueueSize ?? Defaults.MaxQueueCapacity;
-            this.FlushAt = flushAt ?? Defaults.FlushAt;
-            this.Async = async ?? Defaults.Async;
-            this.FlushIntervalInMillis = (int)((flushInterval ?? Defaults.FlushInterval) * 1000);
-            this.Gzip = gzip ?? Defaults.Gzip;
-            this.UserAgent = userAgent ?? Defaults.UserAgent;
-            this.Threads = threads ?? Defaults.Threads;
+            this.Timeout = timeout ?? TimeSpan.FromSeconds(5);
+            this.MaxQueueSize = maxQueueSize;
+            this.FlushAt = flushAt;
+            this.Async = async;
+            this.FlushIntervalInMillis = (int)(flushInterval * 1000);
+            this.Gzip = gzip;
+            this.UserAgent = userAgent ?? GetDefaultUserAgent();
+            this.Threads = threads;
+        }
+
+        private static string GetDefaultUserAgent()
+        {
+            var lib = new Context()["library"] as Dict;
+            return $"{lib["name"]}/{lib["version"]}";
         }
 
         /// <summary>

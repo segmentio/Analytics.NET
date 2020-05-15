35f456ba7dd974fd1f33bcff1ffbfd847c721363
Carlos Galan <cgalan@applaudostudios.com>
2020-05-15 12:58:55 -0600

    LAPP-39 Adding unit tests

--- Test.Net35/Program.cs
+++ Test.Net35/Program.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Segment;
-using Segment.Test;
 
-namespace Test.Net35
+namespace Segment.Test
 {
     class Program
     {

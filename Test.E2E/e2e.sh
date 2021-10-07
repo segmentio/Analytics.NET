306261d061d27ec44f9df39d56540087f4fc7337
imedoc <ignacio.medoc@kinamic.com>
2021-10-07 19:42:01 -0300

    issue#185-Reverted change on the image of linux used to e2e testing

--- Test.E2E/e2e.sh
+++ Test.E2E/e2e.sh
@@ -4,7 +4,7 @@ if [ "$RUN_E2E_TESTS" != "true" ]; then
   echo "Skipping end to end tests."
 else
   echo "Running end to end tests..."
-  wget https://github.com/segmentio/library-e2e-tester/releases/download/0.4.1-pre1/tester_linux_amd64 -O tester
+  wget https://github.com/segmentio/library-e2e-tester/releases/download/0.2.2/tester_linux_amd64 -O tester
   chmod +x tester
 
   # Hide system environment variable values
